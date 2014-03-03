0dca51544cfe296dfc0a2b0b5e96534fb49f32e2
ZRazor <exp.razor@gmail.com>
2014-03-03 23:52:05 +1100

    add first working test websocket chat

---
+++ src/UserData/ChatEndpoint.java
@@ -0,0 +1,24 @@
+package UserData;
+
+/**
+ * Created by razoriii on 03.03.14.
+ */
+import java.io.IOException;
+import javax.websocket.EncodeException;
+import javax.websocket.OnMessage;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ * @author Arun Gupta
+ */
+@ServerEndpoint("/chat")
+public class ChatEndpoint {
+   @OnMessage
+   public void message(String message, Session client) throws IOException, EncodeException {
+      System.out.println("message: " + message);
+      for (Session peer : client.getOpenSessions()) {
+         peer.getBasicRemote().sendText(message);
+      }
+   }
+}
