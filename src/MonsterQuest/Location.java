748b4c3546066658c69ec941d7562dd55057a7ab
apanasenko <alexander.apanasenko@gmail.com>
2014-03-18 21:35:15 +1100

    Fix collision

--- src/MonsterQuest/Location.java
+++ src/MonsterQuest/Location.java
@@ -19,46 +19,39 @@ public class Location {
       return (location.x == this.x && location.y == this.y);
    }
 
-   private static boolean nearCenter(double x, double eps)
-   {
-       return (x + eps > (int) x + 0.5 && x - eps < (int) x + 0.5);
-   }
-
    public Location getNewLocation(Direction direction, double velocity) {
       double eps = 0.15;
+      double left = x - playerSize / 2;
+      double right = x + playerSize / 2;
+      double top = y - playerSize / 2;
+      double bottom = y + playerSize / 2;
+      double vFront = bottom;
+      double hFront = right;
       switch (direction) {
          case NORTH:
-            if (GameMap.canEnterTile((int) (x - playerSize / 2), (int) (y - velocity - playerSize / 2))
-             && GameMap.canEnterTile((int) (x + playerSize / 2), (int) (y - velocity - playerSize / 2)))
-                return new Location(x, y - velocity);
-            else if (nearCenter(x, eps))
-                return new Location((int) x + 0.5, y);
-            else
-                return new Location(x, y);
+            velocity *= -1;
+            vFront = top;
          case SOUTH:
-             if (GameMap.canEnterTile((int) (x - playerSize / 2), (int) (y + velocity + playerSize / 2))
-              && GameMap.canEnterTile((int) (x + playerSize / 2), (int) (y + velocity + playerSize / 2)))
+             if (GameMap.canEnterTile((int)left, (int) (vFront + velocity))
+              && GameMap.canEnterTile((int)right, (int) (vFront + velocity)))
                  return new Location(x, y + velocity);
-             else if (nearCenter(x, eps))
-                 return new Location((int) x + 0.5, y);
-             else
-                 return new Location(x, y);
-         case EAST:
-             if (GameMap.canEnterTile((int) (x + velocity + playerSize / 2), (int) (y - playerSize / 2))
-              && GameMap.canEnterTile((int) (x + velocity + playerSize / 2), (int) (y + playerSize / 2)))
-                 return new Location(x + velocity, y);
-             else if (nearCenter(y, eps))
-                 return new Location(x, (int) y + 0.5);
+             else if (GameMap.canEnterTile((int)(left + eps), (int) (vFront + velocity))
+                   && GameMap.canEnterTile((int)(right - eps), (int) (vFront + velocity)))
+                 return new Location((int)((x + eps) / 0.5 ) * 0.5, y + velocity);
              else
                  return new Location(x, y);
          case WEST:
-             if (GameMap.canEnterTile((int) (x - velocity - playerSize / 2), (int) (y - playerSize / 2))
-              && GameMap.canEnterTile((int) (x - velocity - playerSize / 2), (int) (y + playerSize / 2)))
-                 return new Location(x - velocity, y);
-             else if (nearCenter(y, eps))
-                 return new Location(x, (int) y + 0.5);
-             else
-                 return new Location(x, y);
+            velocity *= -1;
+            hFront = left;
+         case EAST:
+            if (GameMap.canEnterTile((int) (hFront + velocity), (int)top)
+             && GameMap.canEnterTile((int) (hFront + velocity), (int)bottom))
+                return new Location(x + velocity, y);
+            else if (GameMap.canEnterTile((int) (hFront + velocity), (int)(top + eps))
+                  && GameMap.canEnterTile((int) (hFront + velocity), (int)(bottom - eps)))
+                return new Location(x + velocity, (int)((y + eps) / 0.5) * 0.5);
+            else
+                return new Location(x, y);
          case NONE:
             // fall through
          default:
