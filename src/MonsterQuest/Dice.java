33cda8c58f9b3f9c946d43f1a34fe2c5750b86b5
apanasenko <alexander.apanasenko@gmail.com>
2014-03-26 02:55:26 +1100

    Add normal distribution

---
+++ src/MonsterQuest/Dice.java
@@ -0,0 +1,21 @@
+package MonsterQuest;
+
+/**
+ * Created by Alexander on 3/26/14.
+ */
+
+import java.util.Random;
+
+public class Dice {
+
+    private static Random rand = new Random();
+
+    public static int Shoot(int N, int count_shoot){
+        int result = 0;
+        while(count_shoot > 0){
+            result += rand.nextInt(N);
+            count_shoot--;
+        }
+        return result;
+    };
+}
