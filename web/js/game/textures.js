acd1e491d95715aafd9e38bc893c55606ee5c75e
ZRazor <exp.razor@gmail.com>
2014-05-13 18:47:10 +1100

    Nice texture load

--- web/js/game/textures.js
+++ web/js/game/textures.js
@@ -3,34 +3,35 @@ var TILE_SIZE = 32;
 function Textures() {
     this.atlas = PIXI.BaseTexture.fromImage('img/tileset.png');
     this.data = {};
-    this.loadTexture('grass', 22, 16);
-    this.loadTexture('wall', 23, 16);
-    this.loadTexture('inventory', 26, 14);
-    this.loadTexture('player', 0, 2);
-    this.loadTexture('icky thing', 5, 3);
-    this.loadTexture('jelly', 3, 2);
-    this.loadTexture('yeek', 6, 3);
-    this.loadTexture('ghost', 0, 3);
-    this.loadTexture('bat', 1, 5);
-    this.loadTexture('minor demon', 0, 5);
-    this.loadTexture('worm', 7, 2);
-    this.loadTexture('mold', 10, 5);
-    this.loadTexture('armor1', 0, 6);
-    this.loadTexture('armor2', 1, 6);
-    this.loadTexture('armor3', 2, 6);
-    this.loadTexture('armor4', 3, 6);
-    this.loadTexture('axe1', 3, 29);
-    this.loadTexture('axe2', 1, 29);
-    this.loadTexture('ring1', 6, 25);
-    this.loadTexture('ring2', 17, 25);
-    this.loadTexture('ring3', 18, 25);
-    this.loadTexture('staff1', 0, 27);
-    this.loadTexture('staff2', 1, 27);
-    this.loadTexture('staff3', 3, 27);
-    this.loadTexture('gloves1', 13, 21);
+    this.loadTexture(22, 16, 'grass')
+        .loadTexture(23, 16, 'wall')
+        .loadTexture(26, 14, 'inventory')
+        .loadTexture(0, 2, 'player')
+        .loadTexture(5, 3, 'icky thing')
+        .loadTexture(3, 2, 'jelly')
+        .loadTexture(6, 3, 'yeek')
+        .loadTexture(0, 3, 'ghost')
+        .loadTexture(1, 5, 'bat')
+        .loadTexture(0, 5, 'minor demon')
+        .loadTexture(7, 2, 'worm')
+        .loadTexture(10, 5, 'mold')
+        .loadTexture(0, 6, 'armor1')
+        .loadTexture(1, 6, 'armor2')
+        .loadTexture(2, 6, 'armor3')
+        .loadTexture(3, 6, 'armor4')
+        .loadTexture(3, 29, 'axe1')
+        .loadTexture(1, 29, 'axe2')
+        .loadTexture(6, 25, 'ring1')
+        .loadTexture(17, 25, 'ring2')
+        .loadTexture(18, 25, 'ring3')
+        .loadTexture(0, 27, 'staff1')
+        .loadTexture(1, 27, 'staff2')
+        .loadTexture(3, 27, 'staff3')
+        .loadTexture(13, 21, 'gloves1');
 
 }
 
-Textures.prototype.loadTexture = function (name, x, y) {
+Textures.prototype.loadTexture = function (x, y, name) {
     this.data[name] = new PIXI.Texture(this.atlas, new PIXI.Rectangle(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE));
+    return this;
 };
